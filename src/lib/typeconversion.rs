574681944a0ac7d50808ca4e2d23b5279b884598
FrewtyPebbles <48639569+FrewtyPebbles@users.noreply.github.com>
2022-11-12 02:53:31 -0800

    0.2.0

--- src/lib/typeconversion.rs
+++ src/lib/typeconversion.rs
@@ -2,7 +2,7 @@ use super::datatypes::VType;
 
 pub fn determine_type(v_type: VType, raw_type: String) -> VType {
     let mut ret_type: VType = VType::Blank;
-
+    //println!("{}", raw_type);
     match v_type {
         VType::Int(_) => {
 			ret_type = VType::Int(raw_type.parse::<i64>().unwrap());
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,14 @@
-use std::{io::{stdin, self}, env};
+use std::{io::{stdin, self}, env, collections::HashMap};
 
 use lib::tokenizer::serialize;
+use serde::Serialize;
+use serde_json::value::Serializer;
+
+use crate::lib::datatypes::VType;
 
 pub mod lib;
 fn main() {
     let args: Vec<String> = env::args().collect();
-    println!("{}", serialize(&args[1]).unwrap().to_string());
+    let aliases:HashMap<String, Vec<VType>> = HashMap::new();
+    println!("{}", serialize(&args[1], &aliases).serialize(Serializer).unwrap().to_string());
 }
